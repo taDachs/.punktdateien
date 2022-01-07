6e59e2e45bf493555096a651777c66dfb95d0528
Max Schik <max.schik@googlemail.com>
2022-01-07 16:05:18 +0100

    adds install commands for plugin loaders

--- install.sh
+++ install.sh
@@ -7,3 +7,14 @@ stow --target ~/.config nvim
 stow --target ~/.config polybar
 stow --target ~/.config git
 stow --target ~/.config nort
+
+# install plugin loaders for nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# install plugin loaders for zsh
+curl \
+  -sL --create-dirs \
+  https://git.sr.ht/~yerinalexey/miniplug/blob/master/miniplug.zsh \
+  -o $HOME/.local/share/miniplug.zsh
+
