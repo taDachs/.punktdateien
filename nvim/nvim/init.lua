5236b3b1b6ce89af857c7bc2f5d61be5de1b568f
Max Schik <max.schik@googlemail.com>
2023-03-02 02:20:44 +0100

    updates nvim config

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,21 +1,21 @@
 local core = require "core"
 local plugins = require "plugins"
-local lsp = require "lsp"
-local completion = require "completion"
-local ui = require "ui"
-local treesitter = require "treesitter"
-local snippets = require "snippets"
 
-lsp.setup_dependencies()
-completion.setup_dependencies()
-ui.setup_dependencies()
-treesitter.setup_dependencies()
-snippets.setup_dependencies()
+local modules = {
+  "lsp",
+  "completion",
+  "ui",
+  "treesitter",
+  "snippets",
+}
+
+for _, module in pairs(modules) do
+  require(module).setup_dependencies()
+end
 
 core.setup()
 plugins.setup()
-lsp.setup()
-completion.setup()
-ui.setup()
-treesitter.setup()
-snippets.setup()
+
+for _, module in pairs(modules) do
+  require(module).setup()
+end
