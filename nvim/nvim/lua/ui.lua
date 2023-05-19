7f776cddb684c2a8844ee8f4fcbbb7598162486f
Max Schik <max.schik@googlemail.com>
2023-05-19 23:03:57 +0200

    updates dotfiles

--- nvim/nvim/lua/ui.lua
+++ nvim/nvim/lua/ui.lua
@@ -19,118 +19,102 @@ M.dependencies = {
     "nvim-lualine/lualine.nvim",
     dependencies = { "kyazdani42/nvim-web-devicons" },
     config = true,
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = {
-          normal = {
-            a = { bg = M.colors.kit_exclusive_green[1], fg = M.colors.background_color[1], gui = "bold" },
-            b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-            c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[4] },
-          },
-          insert = {
-            a = { bg = M.colors.kit_orange[1], fg = M.colors.background_color[1], gui = "bold" },
-            b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-            c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[4] },
-          },
-          visual = {
-            a = { bg = M.colors.kit_yellow[1], fg = M.colors.background_color[1], gui = "bold" },
-            b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-            c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
-          },
-          replace = {
-            a = { bg = M.colors.kit_red[1], fg = M.colors.background_color[1], gui = "bold" },
-            b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-            c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
-          },
-          command = {
-            a = { bg = M.colors.kit_lila[1], fg = M.colors.background_color[1], gui = "bold" },
-            b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-            c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
-          },
-          inactive = {
-            a = { bg = M.colors.background_color[2], fg = M.colors.foreground_color[2], gui = "bold" },
-            b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-            c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
-          },
-        },
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = {},
-        always_divide_middle = true,
-        globalstatus = false,
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {},
-        lualine_x = {
-          function()
-            local workspace = require("ros-nvim.ros").handle.env:get_current_ws()
-            if workspace == nil then
-              return ""
-            else
-              return workspace
-            end
-          end,
-          "encoding",
-          "fileformat",
-          "filetype",
-        },
-        lualine_y = {
-          "progress",
-          function()
-            return vim.fn.line "." .. "/" .. vim.fn.line "$"
-          end,
-        },
-        lualine_z = { "location" },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {
-        lualine_a = {
-          {
-          "tabs",
-          mode = 1, -- 0: Shows tab_nr
-          }
-        },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {
-          -- { require("nvim-treesitter").statusline(90) }
-        },
-      },
-      extensions = {},
-    },
-  },
-
-  -- colorschemes
-  "phanviet/vim-monokai-pro",
-  {
-    "tadachs/kit.vim",
-    init = function()
-      vim.g.kit_italic = 1
-      vim.g.kit_bold = 1
-    end,
-    dev = true,
+    -- opts = {
+    --   options = {
+    --     icons_enabled = true,
+    --     theme = {
+    --       normal = {
+    --         a = { bg = M.colors.kit_exclusive_green[1], fg = M.colors.background_color[1], gui = "bold" },
+    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[4] },
+    --       },
+    --       insert = {
+    --         a = { bg = M.colors.kit_orange[1], fg = M.colors.background_color[1], gui = "bold" },
+    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[4] },
+    --       },
+    --       visual = {
+    --         a = { bg = M.colors.kit_yellow[1], fg = M.colors.background_color[1], gui = "bold" },
+    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
+    --       },
+    --       replace = {
+    --         a = { bg = M.colors.kit_red[1], fg = M.colors.background_color[1], gui = "bold" },
+    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
+    --       },
+    --       command = {
+    --         a = { bg = M.colors.kit_lila[1], fg = M.colors.background_color[1], gui = "bold" },
+    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
+    --       },
+    --       inactive = {
+    --         a = { bg = M.colors.background_color[2], fg = M.colors.foreground_color[2], gui = "bold" },
+    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
+    --       },
+    --     },
+    --     component_separators = { left = "", right = "" },
+    --     section_separators = { left = "", right = "" },
+    --     disabled_filetypes = {},
+    --     always_divide_middle = true,
+    --     globalstatus = false,
+    --   },
+    --   sections = {
+    --     lualine_a = { "mode" },
+    --     lualine_b = { "branch", "diff", "diagnostics" },
+    --     lualine_c = {},
+    --     lualine_x = {
+    --       function()
+    --         local workspace = require("ros-nvim.ros").handle.env:get_current_ws()
+    --         if workspace == nil then
+    --           return ""
+    --         else
+    --           return workspace
+    --         end
+    --       end,
+    --       "encoding",
+    --       "fileformat",
+    --       "filetype",
+    --     },
+    --     lualine_y = {
+    --       "progress",
+    --       function()
+    --         return vim.fn.line "." .. "/" .. vim.fn.line "$"
+    --       end,
+    --     },
+    --     lualine_z = { "location" },
+    --   },
+    --   inactive_sections = {
+    --     lualine_a = {},
+    --     lualine_b = {},
+    --     lualine_c = { "filename" },
+    --     lualine_x = { "location" },
+    --     lualine_y = {},
+    --     lualine_z = {},
+    --   },
+    --   tabline = {
+    --     lualine_a = {
+    --       {
+    --       "tabs",
+    --       mode = 1, -- 0: Shows tab_nr
+    --       }
+    --     },
+    --     lualine_b = {},
+    --     lualine_c = {},
+    --     lualine_x = {},
+    --     lualine_y = {},
+    --     lualine_z = {
+    --       -- { require("nvim-treesitter").statusline(90) }
+    --     },
+    --   },
+    --   extensions = {},
+    -- },
   },
 }
 
 function M.setup()
-  vim.cmd.colorscheme "kit"
-  vim.cmd([[highlight IndentBlanklineChar guifg=]] .. M.colors.background_color[6] .. [[ gui=nocombine]])
-  vim.cmd([[highlight IndentBlanklineSpaceChar guifg=]] .. M.colors.background_color[6] .. [[ gui=nocombine]])
-  vim.cmd([[highlight IndentBlanklineContextChar guifg=]] .. M.colors.foreground_color[4] .. [[ gui=nocombine]])
-  vim.cmd([[highlight CursorLine guibg=]] .. M.colors.background_color[4])
-  vim.cmd([[highlight CursorLineNr guifg=]] .. M.colors.kit_exclusive_green[1])
+  vim.cmd.colorscheme "habamax"
 
   vim.opt.cursorline = true
   vim.o.signcolumn = "yes"
