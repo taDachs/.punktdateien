164b8b74f92823677feb16d6b3cd113738cd67c8
Max Schik <max.schik@googlemail.com>
2023-05-19 23:20:17 +0200

    updates dotfiles

--- nvim/nvim/lua/ui.lua
+++ nvim/nvim/lua/ui.lua
@@ -15,102 +15,102 @@ M.colors = {
 }
 
 M.dependencies = {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "kyazdani42/nvim-web-devicons" },
-    config = true,
-    -- opts = {
-    --   options = {
-    --     icons_enabled = true,
-    --     theme = {
-    --       normal = {
-    --         a = { bg = M.colors.kit_exclusive_green[1], fg = M.colors.background_color[1], gui = "bold" },
-    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[4] },
-    --       },
-    --       insert = {
-    --         a = { bg = M.colors.kit_orange[1], fg = M.colors.background_color[1], gui = "bold" },
-    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[4] },
-    --       },
-    --       visual = {
-    --         a = { bg = M.colors.kit_yellow[1], fg = M.colors.background_color[1], gui = "bold" },
-    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
-    --       },
-    --       replace = {
-    --         a = { bg = M.colors.kit_red[1], fg = M.colors.background_color[1], gui = "bold" },
-    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
-    --       },
-    --       command = {
-    --         a = { bg = M.colors.kit_lila[1], fg = M.colors.background_color[1], gui = "bold" },
-    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
-    --       },
-    --       inactive = {
-    --         a = { bg = M.colors.background_color[2], fg = M.colors.foreground_color[2], gui = "bold" },
-    --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
-    --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
-    --       },
-    --     },
-    --     component_separators = { left = "", right = "" },
-    --     section_separators = { left = "", right = "" },
-    --     disabled_filetypes = {},
-    --     always_divide_middle = true,
-    --     globalstatus = false,
-    --   },
-    --   sections = {
-    --     lualine_a = { "mode" },
-    --     lualine_b = { "branch", "diff", "diagnostics" },
-    --     lualine_c = {},
-    --     lualine_x = {
-    --       function()
-    --         local workspace = require("ros-nvim.ros").handle.env:get_current_ws()
-    --         if workspace == nil then
-    --           return ""
-    --         else
-    --           return workspace
-    --         end
-    --       end,
-    --       "encoding",
-    --       "fileformat",
-    --       "filetype",
-    --     },
-    --     lualine_y = {
-    --       "progress",
-    --       function()
-    --         return vim.fn.line "." .. "/" .. vim.fn.line "$"
-    --       end,
-    --     },
-    --     lualine_z = { "location" },
-    --   },
-    --   inactive_sections = {
-    --     lualine_a = {},
-    --     lualine_b = {},
-    --     lualine_c = { "filename" },
-    --     lualine_x = { "location" },
-    --     lualine_y = {},
-    --     lualine_z = {},
-    --   },
-    --   tabline = {
-    --     lualine_a = {
-    --       {
-    --       "tabs",
-    --       mode = 1, -- 0: Shows tab_nr
-    --       }
-    --     },
-    --     lualine_b = {},
-    --     lualine_c = {},
-    --     lualine_x = {},
-    --     lualine_y = {},
-    --     lualine_z = {
-    --       -- { require("nvim-treesitter").statusline(90) }
-    --     },
-    --   },
-    --   extensions = {},
-    -- },
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   dependencies = { "kyazdani42/nvim-web-devicons" },
+  --   config = true,
+  --   -- opts = {
+  --   --   options = {
+  --   --     icons_enabled = true,
+  --   --     theme = {
+  --   --       normal = {
+  --   --         a = { bg = M.colors.kit_exclusive_green[1], fg = M.colors.background_color[1], gui = "bold" },
+  --   --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+  --   --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[4] },
+  --   --       },
+  --   --       insert = {
+  --   --         a = { bg = M.colors.kit_orange[1], fg = M.colors.background_color[1], gui = "bold" },
+  --   --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+  --   --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[4] },
+  --   --       },
+  --   --       visual = {
+  --   --         a = { bg = M.colors.kit_yellow[1], fg = M.colors.background_color[1], gui = "bold" },
+  --   --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+  --   --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
+  --   --       },
+  --   --       replace = {
+  --   --         a = { bg = M.colors.kit_red[1], fg = M.colors.background_color[1], gui = "bold" },
+  --   --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+  --   --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
+  --   --       },
+  --   --       command = {
+  --   --         a = { bg = M.colors.kit_lila[1], fg = M.colors.background_color[1], gui = "bold" },
+  --   --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+  --   --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
+  --   --       },
+  --   --       inactive = {
+  --   --         a = { bg = M.colors.background_color[2], fg = M.colors.foreground_color[2], gui = "bold" },
+  --   --         b = { bg = M.colors.background_color[4], fg = M.colors.foreground_color[1] },
+  --   --         c = { bg = M.colors.background_color[3], fg = M.colors.foreground_color[1] },
+  --   --       },
+  --   --     },
+  --   --     component_separators = { left = "", right = "" },
+  --   --     section_separators = { left = "", right = "" },
+  --   --     disabled_filetypes = {},
+  --   --     always_divide_middle = true,
+  --   --     globalstatus = false,
+  --   --   },
+  --   --   sections = {
+  --   --     lualine_a = { "mode" },
+  --   --     lualine_b = { "branch", "diff", "diagnostics" },
+  --   --     lualine_c = {},
+  --   --     lualine_x = {
+  --   --       function()
+  --   --         local workspace = require("ros-nvim.ros").handle.env:get_current_ws()
+  --   --         if workspace == nil then
+  --   --           return ""
+  --   --         else
+  --   --           return workspace
+  --   --         end
+  --   --       end,
+  --   --       "encoding",
+  --   --       "fileformat",
+  --   --       "filetype",
+  --   --     },
+  --   --     lualine_y = {
+  --   --       "progress",
+  --   --       function()
+  --   --         return vim.fn.line "." .. "/" .. vim.fn.line "$"
+  --   --       end,
+  --   --     },
+  --   --     lualine_z = { "location" },
+  --   --   },
+  --   --   inactive_sections = {
+  --   --     lualine_a = {},
+  --   --     lualine_b = {},
+  --   --     lualine_c = { "filename" },
+  --   --     lualine_x = { "location" },
+  --   --     lualine_y = {},
+  --   --     lualine_z = {},
+  --   --   },
+  --   --   tabline = {
+  --   --     lualine_a = {
+  --   --       {
+  --   --       "tabs",
+  --   --       mode = 1, -- 0: Shows tab_nr
+  --   --       }
+  --   --     },
+  --   --     lualine_b = {},
+  --   --     lualine_c = {},
+  --   --     lualine_x = {},
+  --   --     lualine_y = {},
+  --   --     lualine_z = {
+  --   --       -- { require("nvim-treesitter").statusline(90) }
+  --   --     },
+  --   --   },
+  --   --   extensions = {},
+  --   -- },
+  -- },
 }
 
 function M.setup()
