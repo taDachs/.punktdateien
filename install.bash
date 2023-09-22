f494147afe23e278ab781fa24f6be51e0536e462
Max Schik <max.schik@googlemail.com>
2023-09-22 15:47:56 +0200

    updates dotfiles

--- install.bash
+++ install.bash
@@ -26,7 +26,6 @@ if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" 
 fi
 
 if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]] ; then
-  p
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
