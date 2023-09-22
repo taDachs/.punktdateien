536434dfb508d184b8a7210bd03ba9123984ea96
Max Schik <max.schik@googlemail.com>
2023-09-22 14:24:12 +0200

    updates dotfiles

--- install.bash
+++ install.bash
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 set -x
 
-stow --target ~ zsh
-stow --target ~/.config alacritty
-stow --target ~/.config sway
-stow --target ~/.config waybar
-stow --target ~/.config i3
-stow --target ~/.config i3status
-stow --target ~/.config awesome
-stow --target ~/.config nvim
-stow --target ~/.config polybar
-stow --target ~/.config git
-stow --target ~/.config nort
-stow --target ~ tmux
-stow --target ~/.config starship
-stow --target ~/.config gtk-3.0
-stow --target ~/.config rofi
-stow --target ~ shell
-stow --target ~/.config latexmk
+stow --target $HOME zsh
+stow --target $HOME/.config alacritty
+stow --target $HOME/.config sway
+stow --target $HOME/.config waybar
+stow --target $HOME/.config i3
+stow --target $HOME/.config i3status
+stow --target $HOME/.config awesome
+stow --target $HOME/.config nvim
+stow --target $HOME/.config polybar
+stow --target $HOME/.config git
+stow --target $HOME/.config nort
+stow --target $HOME tmux
+stow --target $HOME/.config starship
+stow --target $HOME/.config gtk-3.0
+stow --target $HOME/.config rofi
+stow --target $HOME shell
+stow --target $HOME/.config latexmk
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] ; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]] ; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] ; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]] ; then
+  p
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zlong_alert" ] ; then
-  git clone https://github.com/kevinywlui/zlong_alert.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zlong_alert
+if [[ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zlong_alert" ]] ; then
+  git clone https://github.com/kevinywlui/zlong_alert.zsh ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zlong_alert
 fi
