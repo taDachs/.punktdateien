36e5340ea2d488fbefe9ec11942ef4770e141f64
Max Schik <max.schik@googlemail.com>
2023-09-22 14:19:25 +0200

    updates dotfiles

--- install.bash
+++ install.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -x
 
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" "" --help
+
 stow --target ~ zsh
 stow --target ~/.config alacritty
 stow --target ~/.config sway
@@ -19,10 +21,14 @@ stow --target ~/.config rofi
 stow --target ~ shell
 stow --target ~/.config latexmk
 
-if [ -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] ; then
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] ; then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
-if [ -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] ; then
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] ; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zlong_alert" ] ; then
+  git clone https://github.com/kevinywlui/zlong_alert.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zlong_alert
 fi
