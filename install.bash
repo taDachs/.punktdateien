6d365ab4cf20176a51f5b911c6acbece7e454fe7
Max Schik <max.schik@googlemail.com>
2023-11-01 00:50:43 +0100

    removes unnecessary stuff

--- install.bash
+++ install.bash
@@ -4,17 +4,7 @@ set -ex
 rm -rf $HOME/.zshrc $HOME/.bashrc $HOME/.profile
 
 stow --target $HOME zsh
-stow --target $HOME/.config alacritty
-stow --target $HOME/.config i3
 stow --target $HOME/.config nvim
-stow --target $HOME/.config polybar
-stow --target $HOME/.config git
 stow --target $HOME tmux
 stow --target $HOME/.config starship
-stow --target $HOME/.config xdg
-stow --target $HOME/.config gtk-3.0
-stow --target $HOME/.config rofi
 stow --target $HOME shell
-stow --target $HOME/.config latexmk
-stow --target $HOME/.config zathura
-stow --target $HOME/.config robot_folders
