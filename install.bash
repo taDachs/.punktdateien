2938ebb04a1b830cdd56f2641c5aab2c6f7b5310
Max Schik <max.schik@googlemail.com>
2023-12-10 15:34:11 +0100

    cleans up dotfiles for kitcar nucs

--- install.bash
+++ install.bash
@@ -6,20 +6,9 @@ rm -rf $HOME/.zshrc $HOME/.bashrc $HOME/.profile
 mkdir -p $HOME/.config
 
 stow --target $HOME zsh
-stow --target $HOME/.config alacritty
-stow --target $HOME/.config i3
 stow --target $HOME/.config nvim
-stow --target $HOME/.config polybar
-stow --target $HOME/.config git
 stow --target $HOME tmux
 stow --target $HOME/.config starship
-stow --target $HOME/.config xdg | echo "xdg failed, but it always fails"
-stow --target $HOME/.config gtk-3.0
-stow --target $HOME/.config rofi
 stow --target $HOME shell
-stow --target $HOME/.config latexmk
-stow --target $HOME/.config zathura
-stow --target $HOME/.config robot_folders
-
 mkdir -p $HOME/.local/bin
 stow --target $HOME/.local/bin scripts
