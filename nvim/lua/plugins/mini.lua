3ec5583b24e655dc52100b84322a3820ba3c7846
Max Schik <max.schik@googlemail.com>
2025-01-05 18:37:09 +0100

    neovim: gets rid of mini for commenting

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -30,25 +30,6 @@ return {
         },
       })
 
-      require('mini.comment').setup({
-        -- Module mappings. Use `''` (empty string) to disable one.
-        mappings = {
-          -- Toggle comment (like `gcip` - comment inner paragraph) for both
-          -- Normal and Visual modes
-          comment = '<leader>c',
-
-          -- Toggle comment on current line
-          comment_line = '<leader>cc',
-
-          -- Toggle comment on visual selection
-          comment_visual = '<leader>c',
-
-          -- Define 'comment' textobject (like `dgc` - delete whole comment block)
-          -- Works also in Visual mode if mapping differs from `comment_visual`
-          textobject = '<leader>c',
-        },
-      })
-
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
