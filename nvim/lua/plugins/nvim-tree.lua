81e7b831f0a37d388f2c43c66007e08e535cf46d
Max Schik <max.schik@googlemail.com>
2025-01-05 12:43:58 +0100

    neovim: increase nvim-tree width

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,17 @@
 return {
-  { "nvim-tree/nvim-tree.lua",
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      view = {
+        width = 50,
+      },
+    },
     config = true,
     lazy = false,
     -- opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-      {"<leader>tt", ":NvimTreeFindFileToggle<cr>", mode="n", desc="[T]oggle [T]ree"},
+      { "<leader>tt", ":NvimTreeFindFileToggle<cr>", mode = "n", desc = "[T]oggle [T]ree" },
     }
   },
 }
