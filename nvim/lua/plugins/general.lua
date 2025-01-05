e0ddaf97ad9a3e4ed699461911f1b65b6d2075fb
Max Schik <max.schik@googlemail.com>
2025-01-05 18:36:54 +0100

    neovim: adds snekcase plugin

--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -2,6 +2,14 @@ return {
   -- general QoL stuff
   "tpope/vim-sleuth",
   "tpope/vim-eunuch",
+  {
+    "nicwest/vim-camelsnek",
+    keys = {
+      { "<leader>gc", "<cmd>Camel<cr>", desc = "[G]o [C]amelcase" },
+      { "<leader>gs", "<cmd>Snek<cr>", desc = "[G]o [S]nekcase" },
+    }
+
+  },
   { -- Useful plugin to show you pending keybinds.
     "folke/which-key.nvim",
     event = "VimEnter", -- Sets the loading event to "VimEnter"
