ad9fbe97a092d5b8eaebfab3fef61a5cb0522b48
Max Schik <max.schik@googlemail.com>
2025-02-22 00:17:02 +0100

    shell: adds default bashrc

--- shell/.profile_plugins/container.sh
+++ shell/.profile_plugins/container.sh
@@ -30,6 +30,7 @@ function fzirobdev() {
 }
 
 alias devup='devcontainer up --workspace-folder . --dotfiles-repository=https://github.com/taDachs/.punktdateien.git --dotfiles-install-command=install.bash'
+alias devupnodot='devcontainer up --workspace-folder .'
 alias devnew='devcontainer up --workspace-folder . --dotfiles-repository=https://github.com/taDachs/.punktdateien.git --dotfiles-install-command=install.bash --remove-existing-container'
 alias devrun='devcontainer exec --workspace-folder .'
 alias devsh='devcontainer exec --workspace-folder . zsh'
