1f5853b6909b832c54125fa40916b07da62414a6
Max Schik <max.schik@googlemail.com>
2025-05-21 09:17:37 +0200

    fixes conda sourcing for shell

--- shell/.profile_plugins/conda.sh
+++ shell/.profile_plugins/conda.sh
@@ -6,15 +6,18 @@ elif [ -d "${HOME}/.miniconda3" ]; then
   CONDA_PATH="${HOME}/.miniconda3"
 fi
 
-__conda_setup="$(\'${CONDA_PATH}/bin/conda\' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "${CONDA_PATH}/etc/profile.d/conda.sh" ]; then
-        . "${CONDA_PATH}/etc/profile.d/conda.sh"
+
+if [ -n "$CONDA_PATH" ]; then
+    __conda_setup="$(\'${CONDA_PATH}/bin/conda\' 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
     else
-        export PATH="${CONDA_PATH}/bin:$PATH"
+        if [ -f "${CONDA_PATH}/etc/profile.d/conda.sh" ]; then
+            . "${CONDA_PATH}/etc/profile.d/conda.sh"
+        else
+            export PATH="${CONDA_PATH}/bin:$PATH"
+        fi
     fi
+    unset __conda_setup
 fi
-unset __conda_setup
 # <<< conda initialize <<<
