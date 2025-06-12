5130c7708cc106e3c830cb6d01cfae728481d1fb
Max Schik <max.schik@googlemail.com>
2025-06-12 10:12:42 +0200

    nvim: moves to lualine (again)

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,64 @@
+local function show_macro_recording()
+  local reg = vim.fn.reg_recording()
+  if reg == "" then return "" end -- not recording
+  return "@" .. reg
+end
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = "auto",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      always_show_tabline = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+        refresh_time = 16, -- ~60fps
+        events = {
+          "WinEnter",
+          "BufEnter",
+          "BufWritePost",
+          "SessionLoadPost",
+          "FileChangedShellPost",
+          "VimResized",
+          "Filetype",
+          "CursorMoved",
+          "CursorMovedI",
+          "ModeChanged",
+        },
+      },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { show_macro_recording, },
+      lualine_z = { "searchcount", "location" },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
+  },
+  config = true,
+}
