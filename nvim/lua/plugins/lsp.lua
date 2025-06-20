94911f1c817e2cea27fcc4eab96418e22e85becd
Max Schik <max.schik@googlemail.com>
2025-06-20 17:23:59 +0200

    nvim: fixes lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,7 +20,7 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      { "mason-org/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+      { "mason-org/mason.nvim", opts = {}, config = true }, -- NOTE: Must be loaded before dependants
       "mason-org/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
 
@@ -30,7 +30,6 @@ return {
 
       "saghen/blink.cmp",
     },
-    event = "VeryLazy",
     opts = {
       servers = {
         basedpyright = {
@@ -99,7 +98,7 @@ return {
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map("gd", picker.definition, "[G]oto [D]efinition")
+          map("grd", picker.definition, "[G]oto [D]efinition")
 
           -- Find references for the word under your cursor.
           map("grr", picker.references, "[G]oto [R]eferences")
@@ -108,10 +107,14 @@ return {
           --  Useful when your language has ways of declaring types without an actual implementation.
           map("gri", picker.implementation, "[G]oto [I]mplementation")
 
+          -- WARN: This is not Goto Definition, this is Goto Declaration.
+          --  For example, in C this would take you to the header.
+          map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
           -- Jump to the type of the word under your cursor.
           --  Useful when you"re not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map("<leader>D", picker.type_definition, "Type [D]efinition")
+          map("grt", picker.type_definition, "Type [D]efinition")
 
           -- [ find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
@@ -128,10 +131,6 @@ return {
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           map("gra", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
-
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
-          --  For example, in C this would take you to the header.
-          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
           -- Show full diagnostic in floating window.
           map("<leader>sd", vim.diagnostic.open_float, "[S]how [Diagnostic]")
@@ -189,8 +188,6 @@ return {
         lspconfig[server].setup(config)
       end
 
-      require("mason").setup()
-
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(opts.servers or {})
@@ -201,7 +198,20 @@ return {
       })
       require("mason-tool-installer").setup { ensure_installed = ensure_installed }
 
-      require("mason-lspconfig").setup { automatic_enable = true }
+      require('mason-lspconfig').setup {
+        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+        automatic_installation = false,
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            -- This handles overriding only values explicitly passed
+            -- by the server configuration above. Useful when disabling
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
+            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            require('lspconfig')[server_name].setup(server)
+          end,
+        },
+      }
 
       local diagnostic_jump_ns = vim.api.nvim_create_namespace 'on_diagnostic_jump'
 
